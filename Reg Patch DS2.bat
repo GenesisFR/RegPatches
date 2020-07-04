bc063050235df78edbf42f85c87b5792548fc8cb
David <3614449+GenesisFR@users.noreply.github.com>
2020-07-04 17:36:46 -0400

    + added an option to export the registry entries for Linux users (untested)
    * fixed a bug causing the script to use short filenames when restarting as admin, preventing the directory junction from being created in some cases - removed unnecessary registry entries

--- Reg Patch DS2.bat
+++ Reg Patch DS2.bat
@@ -1,6 +1,6 @@
 @echo off
 
-title Reg Patcher for Dungeon Siege 2 by Genesis (v1.3)
+title Reg Patcher for Dungeon Siege 2 by Genesis (v1.4)
 
 rem https://ss64.com/vb/syntax-elevate.html
 echo Checking if the script is run as admin...
@@ -15,7 +15,7 @@ if %ERRORLEVEL%% == 0 (
     echo The script will now restart as admin.
 
     echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\ElevateMe.vbs"
-    echo UAC.ShellExecute """%~s0""", "", "", "runas", 1 >> "%TEMP%\ElevateMe.vbs"
+    echo UAC.ShellExecute """%~f0""", "", "", "runas", 1 >> "%TEMP%\ElevateMe.vbs"
 
     "%TEMP%\ElevateMe.vbs"
     del "%TEMP%\ElevateMe.vbs"
@@ -29,6 +29,10 @@ rem https://www.codeproject.com/Tips/119828/Running-a-bat-file-as-administrator-
 rem Correct current directory when a script is run as admin
 @setlocal enableextensions
 @cd /d "%~dp0"
+
+rem https://alt.msdos.batch.narkive.com/LNB84uUc/replace-all-backslashes-in-a-string-with-double-backslash
+rem Double backslashes in the current directory path
+set _CD_DOUBLE_BACKSLASH=%CD:\=\\%
 
 rem https://ss64.com/nt/syntax-64bit.html
 echo Detecting OS bitness...
@@ -47,42 +51,39 @@ echo.
 
 rem Shortcuts for registry keys
 set _2K_BW="HKLM\Software\2K Games\Dungeon Siege 2 Broken World"
+set _2K_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\WOW6432Node\2K Games\Dungeon Siege 2 Broken World
 set _GPG_BW_BASE="HKLM\Software\Gas Powered Games\Dungeon Siege 2 Broken World"
 set _GPG_BW="HKLM\Software\Gas Powered Games\Dungeon Siege 2 Broken World\1.00.0000"
+set _GPG_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\WOW6432Node\Gas Powered Games\Dungeon Siege 2 Broken World\1.00.0000
 set _MS_DS2="HKLM\Software\Microsoft\Microsoft Games\DungeonSiege2"
+set _MS_DS2_EXPORT=HKEY_LOCAL_MACHINE\Software\WOW6432Node\Microsoft\Microsoft Games\DungeonSiege2
+set _REG_FILE="Reg_Patch_DS2.reg"
 
 echo Please make a selection:
 echo.
 echo 1. Add registry entries for Dungeon Siege 2 (needed for BW, Elys DS2 and the DS2 Tool Kit)
 echo 2. Add registry entries for Dungeon Siege 2 Broken World (needed for Elys DS2BW)
 echo 3. Create a directory junction in Program Files (useful for GameRanger)
-echo 4. Remove registry entries for both games
-echo 5. Exit
+echo 4. Export registry entries to a REG file (useful on Linux)
+echo 5. Remove registry entries for both games
+echo 6. Exit
 echo.
 echo Note: if you're not sure which option to select, just press 1.
 echo.
-choice /c 12345 /N
+choice /c 123456 /N
 
 IF %ERRORLEVEL% == 1 goto DS2
 IF %ERRORLEVEL% == 2 goto DS2BW
 IF %ERRORLEVEL% == 3 goto junction
-IF %ERRORLEVEL% == 4 goto cleanup
-IF %ERRORLEVEL% == 5 exit /B
+IF %ERRORLEVEL% == 4 goto export
+IF %ERRORLEVEL% == 5 goto cleanup
+IF %ERRORLEVEL% == 6 exit /B
 
 :DS2
 echo Adding registry entries for Dungeon Siege 2...
 
 REG ADD %_MS_DS2% /v "AppPath" /t REG_SZ /d "%CD%" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "CDPath" /t REG_SZ /d "E:\\" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "DistroID" /t REG_DWORD /d "0x0000047c" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "DoubleHash" /t REG_BINARY /d "704f25e4d2b1f8ea" /f /reg:32 > nul
 REG ADD %_MS_DS2% /v "InstallationDirectory" /t REG_SZ /d "%CD%" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "InstalledGroup" /t REG_SZ /d "1" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "LangID" /t REG_DWORD /d "9" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "Launched" /t REG_SZ /d "1" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "PID" /t REG_SZ /d "77033-133-5335624-40332" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "Version" /t REG_SZ /d "2" /f /reg:32 > nul
-REG ADD %_MS_DS2% /v "VersionType" /t REG_SZ /d "RetailVersion" /f /reg:32 > nul
 
 echo DONE
 goto end
@@ -91,28 +92,8 @@ goto end
 echo Adding registry entries for Dungeon Siege 2: Broken World...
 
 REG ADD %_2K_BW% /v "AppPath" /t REG_SZ /d "%CD%" /f /reg:32 > nul
-REG ADD %_2K_BW% /v "DistroID" /t REG_DWORD /d "0x0000047c" /f /reg:32 > nul
 REG ADD %_2K_BW% /v "InstallationDirectory" /t REG_SZ /d "%CD%" /f /reg:32 > nul
-REG ADD %_2K_BW% /v "InstalledGroup" /t REG_SZ /d "1" /f /reg:32 > nul
-REG ADD %_2K_BW% /v "PID" /t REG_SZ /d "0204-993D-D268-A1E2" /f /reg:32 > nul
-
-REG ADD %_GPG_BW% /v "DisplayIcon" /t REG_SZ /d "%CD%\DungeonSiege2.exe" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "DisplayName" /t REG_SZ /d "Dungeon Siege 2 Broken World" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "DisplayVersion" /t REG_SZ /d "1.00.0000" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "InstallDate" /t REG_SZ /d "20180204" /f /reg:32 > nul
 REG ADD %_GPG_BW% /v "InstallLocation" /t REG_SZ /d "%CD%" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "Language" /t REG_DWORD /d "9" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "LogMode" /t REG_DWORD /d "1" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "MajorVersion" /t REG_DWORD /d "1" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "MinorVersion" /t REG_DWORD /d "0" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "NoModify" /t REG_DWORD /d "1" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "NoRemove" /t REG_DWORD /d "0" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "NoRepair" /t REG_DWORD /d "1" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "ProductGuid" /t REG_SZ /d "{A563C4F4-BE36-4956-BA0B-E02BDD9F70D5}" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "Publisher" /t REG_SZ /d "Gas Powered Games" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "RegOwner" /t REG_SZ /d "Killah" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "URLInfoAbout" /t REG_SZ /d "http://www.gaspowered.com" /f /reg:32 > nul
-REG ADD %_GPG_BW% /v "Version" /t REG_DWORD /d "0x01000000" /f /reg:32 > nul
 
 echo DONE
 goto end
@@ -132,6 +113,36 @@ IF %ERRORLEVEL% == 0 (
     echo Warning: do NOT move the directory junction somewhere else as it will also move your entire game directory!
     echo It can safely be renamed or deleted.
 )
+
+goto end
+
+:export
+echo REGEDIT4> %_REG_FILE%
+echo.>> %_REG_FILE%
+
+echo Exporting registry entries for Dungeon Siege 2...
+
+echo [%_MS_DS2_EXPORT%]>> %_REG_FILE%
+echo "AppPath"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "InstallationDirectory"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo.>> %_REG_FILE%
+
+echo DONE
+
+echo Exporting registry entries for Dungeon Siege 2: Broken World...
+
+echo [%_2K_BW_EXPORT%]>> %_REG_FILE%
+echo "AppPath"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "InstallationDirectory"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo.>> %_REG_FILE%
+
+echo [%_GPG_BW_EXPORT%]>> %_REG_FILE%
+echo "InstallLocation"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo.>> %_REG_FILE%
+
+echo DONE
+echo.
+echo A new file called %_REG_FILE% has been created in the current directory.
 
 goto end
 

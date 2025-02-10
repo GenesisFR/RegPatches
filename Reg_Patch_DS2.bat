d6e695602a1687c2328948c64cd16c469626a359
David <3614449+GenesisFR@users.noreply.github.com>
2025-02-09 20:00:21 -0500

    + added executable and install directory detection

--- Reg_Patch_DS2.bat
+++ Reg_Patch_DS2.bat
@@ -1,12 +1,13 @@
 @echo off
 @setlocal enableextensions
 
-title Reg Patcher for Dungeon Siege 2 by Genesis (v1.44)
+title Reg Patcher for Dungeon Siege 2 by Genesis (v1.45)
 
-rem Checking if run from Linux
+:linux_check
+rem Check if run from Linux
 if defined WINEPREFIX goto init
 
-rem Checking and validating arguments
+rem Check and validating arguments
 if not "%1" == "" (
     rem If one argument is specified, it must be "-c"
     if "%1" == "-c" (
@@ -30,6 +31,7 @@ if not "%1" == "" (
     )
 )
 
+:admin_check
 rem https://ss64.com/vb/syntax-elevate.html
 rem Restart the script as admin if it wasn't the case already
 echo Checking if the script is run as admin...
@@ -58,12 +60,7 @@ rem https://www.codeproject.com/Tips/119828/Running-a-bat-file-as-administrator-
 rem Correct current directory when a script is run as admin
 @cd /d "%~dp0"
 
-rem https://alt.msdos.batch.narkive.com/LNB84uUc/replace-all-backslashes-in-a-string-with-double-backslash
-rem Double backslashes in the current directory path
-set _CD_DOUBLE_BACKSLASH=%CD:\=\\%
-
 rem https://ss64.com/nt/syntax-64bit.html
-echo Detecting OS bitness...
 set _OS_BITNESS=64
 set _PROGRAM_FILES=%PROGRAMFILES(X86)%
 
@@ -74,10 +71,7 @@ if %PROCESSOR_ARCHITECTURE% == x86 (
     )
 )
 
-echo Your OS is %_OS_BITNESS%-bit.
-echo.
-
-rem Shortcuts for registry keys
+rem Shortcuts for registry stuff
 set _2K_BW=HKLM\Software\2K Games\Dungeon Siege 2 Broken World
 set _2K_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\Wow6432Node\2K Games\Dungeon Siege 2 Broken World
 set _GPG_BW=HKLM\Software\Gas Powered Games\Dungeon Siege 2 Broken World
@@ -87,6 +81,7 @@ set _MS_DS2_EXPORT=HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Microsoft G
 set _REG_ARG=/reg:32
 set _REG_FILE=%~n0.reg
 
+rem WOW6432Node and /reg:32 aren't present on 32-bit systems
 if %_OS_BITNESS% == 32 (
     set _2K_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\2K Games\Dungeon Siege 2 Broken World
     set _GPG_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\Gas Powered Games\Dungeon Siege 2 Broken World\1.00.0000
@@ -99,6 +94,44 @@ if defined WINEPREFIX (
     set _GPG_BW_EXPORT=HKEY_LOCAL_MACHINE\Software\Gas Powered Games\Dungeon Siege 2 Broken World\1.00.0000
     set _MS_DS2_EXPORT=HKEY_LOCAL_MACHINE\Software\Microsoft\Microsoft Games\DungeonSiege2
     set _REG_ARG=
+)
+
+:exe_check
+rem Check for the game executable in the current directory
+echo Checking for the game executable...
+
+if exist DungeonSiege2.exe (
+    set _INSTALL_LOCATION=%CD%
+    echo OK
+    goto menu
+) else (
+    echo DungeonSiege2.exe not found in the current directory!
+)
+
+:install_detection
+rem Check where the game is installed from the registry
+echo.
+echo Searching for the game installation directory...
+
+for /F "tokens=2* delims=	 " %%A in (' REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 39200" /v InstallLocation 2^>nul') do set _INSTALL_LOCATION=%%B
+
+if "%_INSTALL_LOCATION%" == "" (
+    echo.
+    echo No game installation directory found!
+    goto end
+) else (
+    echo Game installation directory found: %_INSTALL_LOCATION%
+
+    rem Check for the game executable in the installation directory
+    echo Checking for the game executable...
+
+    if exist "%_INSTALL_LOCATION%\DungeonSiege2.exe" (
+        echo OK
+        goto menu
+    ) else (
+        echo DungeonSiege2.exe not found in the installation directory!
+        goto end
+    )
 )
 
 rem Selection menu
@@ -131,8 +164,8 @@ if %ERRORLEVEL% == 6 exit /B
 :DS2
 echo Adding registry entries for Dungeon Siege 2...
 
-REG ADD "%_MS_DS2%" /v "AppPath" /t REG_SZ /d "%CD%" /f %_REG_ARG% > nul
-REG ADD "%_MS_DS2%" /v "InstallationDirectory" /t REG_SZ /d "%CD%" /f %_REG_ARG% > nul
+REG ADD "%_MS_DS2%" /v "AppPath" /t REG_SZ /d "%_INSTALL_LOCATION%" /f %_REG_ARG% > nul
+REG ADD "%_MS_DS2%" /v "InstallationDirectory" /t REG_SZ /d "%_INSTALL_LOCATION%" /f %_REG_ARG% > nul
 REG ADD "%_MS_DS2%" /v "PID" /t REG_SZ /d "77033-133-5335624-40332" /f %_REG_ARG% > nul
 
 echo DONE
@@ -141,10 +174,10 @@ goto end
 :DS2BW
 echo Adding registry entries for Dungeon Siege 2: Broken World...
 
-REG ADD "%_2K_BW%" /v "AppPath" /t REG_SZ /d "%CD%" /f %_REG_ARG% > nul
-REG ADD "%_2K_BW%" /v "InstallationDirectory" /t REG_SZ /d "%CD%" /f %_REG_ARG% > nul
+REG ADD "%_2K_BW%" /v "AppPath" /t REG_SZ /d "%_INSTALL_LOCATION%" /f %_REG_ARG% > nul
+REG ADD "%_2K_BW%" /v "InstallationDirectory" /t REG_SZ /d "%_INSTALL_LOCATION%" /f %_REG_ARG% > nul
 REG ADD "%_2K_BW%" /v "PID" /t REG_SZ /d "0204-993D-D268-A1E2" /f %_REG_ARG% > nul
-REG ADD "%_GPG_BW%\1.00.0000" /v "InstallLocation" /t REG_SZ /d "%CD%" /f %_REG_ARG% > nul
+REG ADD "%_GPG_BW%\1.00.0000" /v "InstallLocation" /t REG_SZ /d "%_INSTALL_LOCATION%" /f %_REG_ARG% > nul
 
 echo DONE
 goto end
@@ -152,10 +185,10 @@ goto end
 :junction
 rem https://stackoverflow.com/a/8071683
 rem Get the current directory name
-for %%a in (.) do set _CURRENT_DIRECTORY=%%~nxa
+for %%a in ("%_INSTALL_LOCATION%") do set _CURRENT_DIRECTORY=%%~nxa
 
 if exist "%_PROGRAM_FILES%\%_CURRENT_DIRECTORY%" rmdir /Q "%_PROGRAM_FILES%\%_CURRENT_DIRECTORY%" > nul
-mklink /J "%_PROGRAM_FILES%\%_CURRENT_DIRECTORY%" "%CD%"
+mklink /J "%_PROGRAM_FILES%\%_CURRENT_DIRECTORY%" "%_INSTALL_LOCATION%"
 
 if %ERRORLEVEL% == 0 (
     echo.
@@ -168,14 +201,18 @@ if %ERRORLEVEL% == 0 (
 goto end
 
 :export
+rem https://alt.msdos.batch.narkive.com/LNB84uUc/replace-all-backslashes-in-a-string-with-double-backslash
+rem Double backslashes in the install directory path
+set _INSTALL_LOCATION_DOUBLE_BACKSLASH=%_INSTALL_LOCATION:\=\\%
+
 echo REGEDIT4> %_REG_FILE%
 echo.>> %_REG_FILE%
 
 echo Exporting registry entries for Dungeon Siege 2...
 
 echo [%_MS_DS2_EXPORT%]>> %_REG_FILE%
-echo "AppPath"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
-echo "InstallationDirectory"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "AppPath"="%_INSTALL_LOCATION_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "InstallationDirectory"="%_INSTALL_LOCATION_DOUBLE_BACKSLASH%">> %_REG_FILE%
 echo.>> %_REG_FILE%
 
 echo DONE
@@ -183,12 +220,12 @@ echo.
 echo Exporting registry entries for Dungeon Siege 2: Broken World...
 
 echo [%_2K_BW_EXPORT%]>> %_REG_FILE%
-echo "AppPath"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
-echo "InstallationDirectory"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "AppPath"="%_INSTALL_LOCATION_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "InstallationDirectory"="%_INSTALL_LOCATION_DOUBLE_BACKSLASH%">> %_REG_FILE%
 echo.>> %_REG_FILE%
 
 echo [%_GPG_BW_EXPORT%]>> %_REG_FILE%
-echo "InstallLocation"="%_CD_DOUBLE_BACKSLASH%">> %_REG_FILE%
+echo "InstallLocation"="%_INSTALL_LOCATION_DOUBLE_BACKSLASH%">> %_REG_FILE%
 echo.>> %_REG_FILE%
 
 echo DONE

0899bed40ed2d6abafc408af82fd51fe9062d3a2
David <3614449+GenesisFR@users.noreply.github.com>
2025-12-31 14:59:55 -0500

    * fixed "unexpected )" error for DS1 on Linux

--- Reg_Patch_DS1.bat
+++ Reg_Patch_DS1.bat
@@ -1,7 +1,7 @@
 @echo off
 @setlocal enableextensions
 
-title Reg Patcher for Dungeon Siege 1 by Genesis (v1.47)
+title Reg Patcher for Dungeon Siege 1 by Genesis (v1.48)
 
 :linux_check
 rem Check if run from Linux
@@ -55,12 +55,12 @@ rem Correct the current directory when a script is run as admin
 
 rem https://ss64.com/nt/syntax-64bit.html
 set _OS_BITNESS=64
-set _PROGRAM_FILES=%PROGRAMFILES(X86)%
+set _PROGRAM_FILES="%PROGRAMFILES(X86)%"
 
 if %PROCESSOR_ARCHITECTURE% == x86 (
     if not defined PROCESSOR_ARCHITEW6432 (
         set _OS_BITNESS=32
-        set _PROGRAM_FILES=%PROGRAMFILES%
+        set _PROGRAM_FILES="%PROGRAMFILES%"
     )
 )
 

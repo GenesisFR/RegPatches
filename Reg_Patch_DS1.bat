GenesisFR/RegPatches

commit fbbabe7633dccd7b2ec5b126277b65aa8a4cd275

- merged the "read_ini" and "append_mp_section" subroutines

Reg_Patch_DS1.bat
@@ -271,8 +271,6 @@ if exist "%_CFG_FILE_DS%" (
 	setlocal EnableDelayedExpansion
 	call :read_ini "%_CFG_FILE_DS%"
 	setlocal DisableDelayedExpansion
-
-	call :append_mp_section "%_CFG_FILE_DS%"
 )
 
 rem Update the LOA config file if it exists
@@ -282,8 +280,6 @@ if exist "%_CFG_FILE_LOA%" (
 	setlocal EnableDelayedExpansion
 	call :read_ini "%_CFG_FILE_LOA%"
 	setlocal DisableDelayedExpansion
-
-	call :append_mp_section "%_CFG_FILE_LOA%"
 )
 
 if %_CFG_FILE_FOUND% EQU 0 (
@@ -294,9 +290,9 @@ if %_CFG_FILE_FOUND% EQU 0 (
 
 goto end
 
+:read_ini
 rem https://tutorialreference.com/batch-scripting/examples/faq/batch-script-how-to-read-and-write-to-an-ini-file
 rem Store the beginning of the config file to a temp file
-:read_ini
 set "_CFG_FILE=%~1"
 set "_CFG_FILE_TEMP=%~1.tmp"
 set "_TARGET_SECTION=multiplayer"
@@ -318,41 +314,29 @@ for /F "usebackq eol=# delims=" %%L in ("%_CFG_FILE%") do (
 
 	rem Write the current line to the temp file until we reach the MP section
 	if !_IN_SECTION! EQU 0 (
-		if "!_LINE!" == "" (
-			echo:>> "%_CFG_FILE_TEMP%"
-		) else (
-			echo !line!>> "%_CFG_FILE_TEMP%"
-		)
+		if not "!_LINE!" == "" ( echo !_LINE!>> "%_CFG_FILE_TEMP%" )
 	) else (
+		rem Append the MP section to the temp file
+		(
+			echo:
+			echo [multiplayer]
+			echo gun_server = gz.exsurge.net
+			echo gun_server_port = 2300
+			echo news_server = gz.exsurge.net
+			echo news_server_port = 2301
+			echo news_server_file = news.txt
+			echo autoupdate_server = gz.exsurge.net
+			echo autoupdate_proxy = gz.exsurge.net
+			echo:
+			echo [debug]
+		) >> "%_CFG_FILE_TEMP%"
+
+		rem Overwrite the original config file
+		move /Y "%_CFG_FILE_TEMP%" "%_CFG_FILE%" > nul
+
 		exit /B
 	)
 )
-
-exit /B
-
-rem Append the MP section to the temp file
-:append_mp_section
-set "_CFG_FILE=%~1"
-set "_CFG_FILE_TEMP=%~1.tmp"
-
-rem echo append_mp_section _CFG_FILE = %_CFG_FILE%
-rem echo append_mp_section _CFG_FILE_TEMP = %_CFG_FILE_TEMP%
-
-(
-	echo [multiplayer]
-	echo gun_server = gz.exsurge.net
-	echo gun_server_port = 2300
-	echo news_server = gz.exsurge.net
-	echo news_server_port = 2301
-	echo news_server_file = news.txt
-	echo autoupdate_server = gz.exsurge.net
-	echo autoupdate_proxy = gz.exsurge.net
-	echo:
-	echo [debug]
-) >> "%_CFG_FILE_TEMP%"
-
-rem Overwrite the original config file
-move /Y "%_CFG_FILE_TEMP%" "%_CFG_FILE%" > nul
 
 exit /B
 

commit 2eb4d70a914c98e580c3e50e5ad61bbc3ca01339

* fixed a regression in fbbabe7 where the config file wouldn't get overwritten on Linux
* fixed a regression in fbbabe7 where an extra space was added at the end of every line

Reg_Patch_DS1.bat
@@ -269,8 +269,11 @@ if exist "%_CFG_FILE_DS%" (
 	set "_CFG_FILE_FOUND=1"
 
 	setlocal EnableDelayedExpansion
-	call :read_ini "%_CFG_FILE_DS%"
+	call :edit_ini "%_CFG_FILE_DS%"
 	setlocal DisableDelayedExpansion
+
+	rem Overwrite the original config file
+	move /Y "%_CFG_FILE_DS%.tmp" "%_CFG_FILE_DS%" > nul
 )
 
 rem Update the LOA config file if it exists
@@ -278,8 +281,11 @@ if exist "%_CFG_FILE_LOA%" (
 	set "_CFG_FILE_FOUND=1"
 
 	setlocal EnableDelayedExpansion
-	call :read_ini "%_CFG_FILE_LOA%"
+	call :edit_ini "%_CFG_FILE_LOA%"
 	setlocal DisableDelayedExpansion
+
+	rem Overwrite the original config file
+	move /Y "%_CFG_FILE_LOA%.tmp" "%_CFG_FILE_LOA%" > nul
 )
 
 if %_CFG_FILE_FOUND% EQU 0 (
@@ -290,7 +296,7 @@ if %_CFG_FILE_FOUND% EQU 0 (
 
 goto end
 
-:read_ini
+:edit_ini
 rem https://tutorialreference.com/batch-scripting/examples/faq/batch-script-how-to-read-and-write-to-an-ini-file
 rem Store the beginning of the config file to a temp file
 set "_CFG_FILE=%~1"
@@ -308,13 +314,13 @@ for /F "usebackq eol=# delims=" %%L in ("%_CFG_FILE%") do (
 	rem Section detection logic
 	if "!_LINE:~0,1!"=="[" if "!_LINE:~-1!"=="]" (
 		for /F "delims=[]" %%S in ("!_LINE!") do (
-			if /I "%%S"=="%_TARGET_SECTION%" ( set "_IN_SECTION=1" )
+			if /I "%%S"=="%_TARGET_SECTION%" (set "_IN_SECTION=1")
 		)
 	)
 
 	rem Write the current line to the temp file until we reach the MP section
 	if !_IN_SECTION! EQU 0 (
-		if not "!_LINE!" == "" ( echo !_LINE!>> "%_CFG_FILE_TEMP%" )
+		if not "!_LINE!" == "" (echo !_LINE!>> "%_CFG_FILE_TEMP%")
 	) else (
 		rem Append the MP section to the temp file
 		(
@@ -330,9 +336,6 @@ for /F "usebackq eol=# delims=" %%L in ("%_CFG_FILE%") do (
 			echo:
 			echo [debug]
 		) >> "%_CFG_FILE_TEMP%"
-
-		rem Overwrite the original config file
-		move /Y "%_CFG_FILE_TEMP%" "%_CFG_FILE%" > nul
 
 		exit /B
 	)
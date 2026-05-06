GenesisFR/RegPatches

commit e4caa690047ba79337ee8ff88c7caae421680273

- replaced the steam_install_detection and gog_install_detection subroutines by a function

Reg_Patch_DS1.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-title Reg Patcher for Dungeon Siege 1 by Genesis (v1.55)
+title Reg Patcher for Dungeon Siege 1 by Genesis (v1.56)
 echo You can find the latest version or report issues at https://github.com/GenesisFR/RegPatches.
 echo:
 
@@ -129,59 +129,12 @@ if exist DungeonSiege.exe (
 	echo DungeonSiege.exe and DSLOA.exe not found in the current directory!
 )
 
-:steam_install_detection
-rem Check where the game is installed from the registry
-echo:
-echo Searching for the game Steam installation directory...
-
-for /F "tokens=2*" %%A in ('reg query "%_REG_KEY_STEAM%" /v InstallLocation 2^>nul') do set "_INSTALL_LOCATION=%%B"
-
-if "%_INSTALL_LOCATION%"=="" (
-	echo No Steam installation directory found!
-) else (
-	echo Steam installation directory found: %_INSTALL_LOCATION%
-
-	rem Check for game executables in the installation directory
-	echo Checking for the game executable...
-
-	if exist "%_INSTALL_LOCATION%\DungeonSiege.exe" (
-		echo OK
-		goto menu
-	) else if exist "%_INSTALL_LOCATION%\DSLOA.exe" (
-		echo OK
-		goto menu
-	) else (
-		echo DungeonSiege.exe and DSLOA.exe not found in the game Steam installation directory!
-	)
-)
-
-:gog_install_detection
-rem Check where the game is installed from the registry
-echo:
-echo Searching for the game GOG installation directory...
-
-for /F "tokens=2*" %%A in ('reg query "%_REG_KEY_GOG%" /v path 2^>nul') do set "_INSTALL_LOCATION=%%B"
-
-if "%_INSTALL_LOCATION%"=="" (
-	echo No GOG installation directory found!
-	goto end
-) else (
-	echo GOG installation directory found: %_INSTALL_LOCATION%
-
-	rem Check for the game executable in the installation directory
-	echo Checking for the game executable...
-
-	if exist "%_INSTALL_LOCATION%\DungeonSiege.exe" (
-		echo OK
-		goto menu
-	) else if exist "%_INSTALL_LOCATION%\DSLOA.exe" (
-		echo OK
-		goto menu
-	) else (
-		echo DungeonSiege.exe and DSLOA.exe not found in the game GOG installation directory!
-		goto end
-	)
-)
+rem Check for the game executables in the Steam installation directory, then GOG if not found
+call :install_detection Steam "%_REG_KEY_STEAM%" InstallLocation
+if %ERRORLEVEL%==100 call :install_detection GOG "%_REG_KEY_GOG%" path
+if %ERRORLEVEL%==100 goto end
+rem We've been through the menu, exit
+if %ERRORLEVEL% LEQ 10 goto :eof
 
 :menu
 rem Selection menu
@@ -245,6 +198,34 @@ if not defined _LINUX (
 
 rem This is only here to help with development since we should never reach this in practice
 echo No valid choice detected! & goto end
+
+:install_detection
+rem Check where the game is installed from the registry
+echo:
+echo Searching for the %1 installation directory...
+
+for /F "tokens=2*" %%A in ('reg query %2 /v %3 2^>nul') do set "_INSTALL_LOCATION=%%B"
+
+if "%_INSTALL_LOCATION%"=="" (
+	echo No %1 installation directory found!
+	exit /B 100
+) else (
+	echo %1 installation directory found: %_INSTALL_LOCATION%
+
+	rem Check for game executables in the installation directory
+	echo Checking for the game executable...
+
+	if exist "%_INSTALL_LOCATION%\DungeonSiege.exe" (
+		echo OK
+		goto menu
+	) else if exist "%_INSTALL_LOCATION%\DSLOA.exe" (
+		echo OK
+		goto menu
+	) else (
+		echo DungeonSiege.exe and DSLOA.exe not found in the %1 installation directory!
+		exit /B 100
+	)
+)
 
 :ds1
 echo Adding registry entries for Dungeon Siege...

commit b26260668aab237c2ff726de158407af9b366c89

* fixed a regression from e4caa69 where the reg patch wasn't restarting when selecting option 6

Reg_Patch_DS1.bat
@@ -275,7 +275,7 @@ echo Redirecting the ZoneMatch server to OpenZone...
 rem Add cmd.exe to the Controlled Folder Access whitelist (otherwise the attrib/move commands won't work)
 if not defined _LINUX (
 	if %_WINVER% GEQ 10 (
-		rem Check the registry if it's already been whitelisted
+		rem Check in the registry if it's already been whitelisted
 		for /f "tokens=2*" %%A in ('reg query "HKLM\Software\Microsoft\Windows Defender\Windows Defender Exploit Guard\Controlled Folder Access\AllowedApplications" /v "%SystemRoot%\system32\cmd.exe" 2^>nul') do set "_IS_CMD_ALLOWED=%%B"
 
 		if not defined _IS_CMD_ALLOWED (
@@ -291,7 +291,7 @@ if not defined _LINUX (
 			cls
 
 			rem Restart the reg patch using the same option because Controlled Folder Access changes don't come into effect in the current session
-			cmd /c %0 -c 6
+			cmd /c "%~f0" -c 6
 			exit /B
 		)
 	)